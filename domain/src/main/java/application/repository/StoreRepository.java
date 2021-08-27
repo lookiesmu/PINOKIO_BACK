528505296bf06ef93a48ba79a6e9bec24280ce33
rlaehdals <rkdlem48@gmail.com>
2021-08-27 13:43:42 +0900

    menu 반환 완성

--- domain/src/main/java/application/repository/StoreRepository.java
+++ domain/src/main/java/application/repository/StoreRepository.java
@@ -1,11 +1,17 @@
 package application.repository;
 
 
+import application.domain.Category;
 import application.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StoreRepository extends JpaRepository<Store, Long>{
 
+    Optional<Store> findCategoryByBranchAndStoreName(String branch, String storeName);
 }
--- domain/src/main/java/application/service/StoreServiceImpl.java
+++ domain/src/main/java/application/service/StoreServiceImpl.java
@@ -1,13 +1,27 @@
 package application.service;
 
+import application.domain.Category;
+import application.domain.Store;
+import application.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class StoreServiceImpl {
+public class StoreServiceImpl implements StoreService{
 
+    private final StoreRepository storeRepository;
+
+    @Override
+    public List<Category> findStoreCategory(String storeName, String branch) {
+        Optional<Store> result = storeRepository.findCategoryByBranchAndStoreName(storeName, branch);
+        Store store = result.get();
+        return store.getCategories();
+    }
 }
