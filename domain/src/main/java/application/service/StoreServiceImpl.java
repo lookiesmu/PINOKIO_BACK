9a3e4921044caa212b4c2c451bb9c1d1a83def42
rlaehdals <rkdlem48@gmail.com>
2021-08-27 15:33:10 +0900

    Revert "menu 반환 완성"

--- domain/src/main/java/application/service/StoreServiceImpl.java
+++ domain/src/main/java/application/service/StoreServiceImpl.java
@@ -1,27 +1,13 @@
 package application.service;
 
-import application.domain.Category;
-import application.domain.Store;
-import application.repository.StoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.Optional;
 
 
 @Service
 @RequiredArgsConstructor
 @Transactional
-public class StoreServiceImpl implements StoreService{
+public class StoreServiceImpl {
 
-    private final StoreRepository storeRepository;
-
-    @Override
-    public List<Category> findStoreCategory(String storeName, String branch) {
-        Optional<Store> result = storeRepository.findCategoryByBranchAndStoreName(storeName, branch);
-        Store store = result.get();
-        return store.getCategories();
-    }
 }
